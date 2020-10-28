62a56c9228634ff93d8b5c051f8158cb1c924dbf
liujinye <liujinye@xiaomi.com>
2020-10-28 12:53:54 +0800

    Allow caller to specify key CN in generate_attestation_from_EVP().
    This is needed to generate attestations in the default Identity
    Credential HAL implementation which uses software KM.
    
    Test: atest VtsHalIdentityTargetTest
    Bug: 171745570
    Change-Id: Ic96d1d2362851edca562c5defa1e48bdd0e6e060
    Merged-In: I93b6cb939972c214dc67250dd62aa4fdd76273d6

--- android_keymaster/keymaster_enforcement.cpp
+++ android_keymaster/keymaster_enforcement.cpp
@@ -371,21 +371,16 @@ keymaster_error_t KeymasterEnforcement::AuthorizeBegin(const keymaster_purpose_t
         case KM_TAG_APPLICATION_ID:
         case KM_TAG_OS_VERSION:
         case KM_TAG_OS_PATCHLEVEL:
-        case KM_TAG_BOOT_PATCHLEVEL:
-        case KM_TAG_VENDOR_PATCHLEVEL:
-        case KM_TAG_STORAGE_KEY:
 
         /* Ignored pending removal */
         case KM_TAG_ALL_USERS:
 
-        /* Tags that are not enforced by begin */
+        /* TODO(swillden): Handle these */
         case KM_TAG_INCLUDE_UNIQUE_ID:
         case KM_TAG_UNIQUE_ID:
         case KM_TAG_RESET_SINCE_ID_ROTATION:
         case KM_TAG_ALLOW_WHILE_ON_BODY:
         case KM_TAG_TRUSTED_CONFIRMATION_REQUIRED:
-        case KM_TAG_TRUSTED_USER_PRESENCE_REQUIRED:
-        case KM_TAG_CONFIRMATION_TOKEN:
             break;
 
         case KM_TAG_IDENTITY_CREDENTIAL_KEY:
@@ -463,7 +458,7 @@ bool KeymasterEnforcement::GetAndValidateAuthToken(const AuthorizationSet& opera
         return false;
     }
 
-    if (auth_token_blob.data_length != sizeof(**auth_token)) {
+    if (auth_token_blob.data_length != sizeof(*auth_token)) {
         LOG_E("Bug: Auth token is the wrong size (%d expected, %d found)", sizeof(hw_auth_token_t),
               auth_token_blob.data_length);
         return false;

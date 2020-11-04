464f7b5b746ca87676c665e853b0b74716030b03
openvela-robot <openvela-robot@xiaomi.com>
2020-11-04 21:11:41 +0800

    Merge "Add certificate_utils."

--- tests/attestation_record_test.cpp
+++ tests/attestation_record_test.cpp
@@ -40,7 +40,6 @@ class TestContext : public AttestationRecordContext {
         return KM_ERROR_OK;
     }
     keymaster_error_t GetVerifiedBootParams(keymaster_blob_t* verified_boot_key,
-                                            keymaster_blob_t* /* verified_boot_hash */,
                                             keymaster_verified_boot_t* verified_boot_state,
                                             bool* device_locked) const override {
         verified_boot_key->data = vboot_key_;

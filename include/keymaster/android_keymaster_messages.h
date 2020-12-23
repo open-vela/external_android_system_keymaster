a6fdf0edb2369b25e99d9ebac4a4757d1d465b2d
openvela-robot <openvela-robot@xiaomi.com>
2020-12-23 21:06:16 +0800

    Change GenerateAttestation to use managed cert type. am: a6a4f57064 am: 610ebd9134 am: 771e5d1b41
    Original change: https://android-review.googlesource.com/c/platform/system/keymaster/+/1527559
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: I7989d16bea57005101361f611b01486ca9a625f7

--- include/keymaster/android_keymaster_messages.h
+++ include/keymaster/android_keymaster_messages.h
@@ -624,13 +624,19 @@ struct AttestKeyRequest : public KeymasterMessage {
 };
 
 struct AttestKeyResponse : public KeymasterResponse {
-    explicit AttestKeyResponse(int32_t ver = kDefaultMessageVersion) : KeymasterResponse(ver) {}
+    explicit AttestKeyResponse(int32_t ver = kDefaultMessageVersion) : KeymasterResponse(ver) {
+        certificate_chain.entry_count = 0;
+        certificate_chain.entries = nullptr;
+    }
+    ~AttestKeyResponse();
+
+    bool AllocateChain(size_t entry_count);
 
     size_t NonErrorSerializedSize() const override;
     uint8_t* NonErrorSerialize(uint8_t* buf, const uint8_t* end) const override;
     bool NonErrorDeserialize(const uint8_t** buf_ptr, const uint8_t* end) override;
 
-    CertificateChain certificate_chain;
+    keymaster_cert_chain_t certificate_chain;
 };
 
 struct UpgradeKeyRequest : public KeymasterMessage {

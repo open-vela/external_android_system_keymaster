ba1266eabe1c6276c402b16ef0c66d2711bd4510
openvela-robot <openvela-robot@xiaomi.com>
2021-04-27 17:12:15 +0800

    Merge "Align KeyMint AIDL with usage" am: f3f465d6a7
    Original change: https://android-review.googlesource.com/c/platform/system/keymaster/+/1679931
    
    Change-Id: I7579eeb651793a07540808f3586698d41c4efa2e

--- include/keymaster/UniquePtr.h
+++ include/keymaster/UniquePtr.h
@@ -73,7 +73,8 @@ template <typename T, typename D = DefaultDelete<T>> class UniquePtr {
     T* operator->() const { return mPtr; }
     T* get() const { return mPtr; }
 
-    explicit operator bool() const { return mPtr != nullptr; }
+    // NOLINTNEXTLINE(google-explicit-constructor)
+    operator bool() const { return mPtr != nullptr; }
 
     // Returns the raw pointer and hands over ownership to the caller.
     // The pointer will not be deleted by UniquePtr.
@@ -134,7 +135,8 @@ template <typename T, typename D> class UniquePtr<T[], D> {
         return result;
     }
 
-    explicit operator bool() const { return mPtr != nullptr; }
+    // NOLINTNEXTLINE(google-explicit-constructor)
+    operator bool() const { return mPtr != nullptr; }
 
     void reset(T* ptr = nullptr) {
         if (ptr != mPtr) {

2ebe0a63c8ef05d12af5847680e344b86b4dfbc7
openvela-robot <openvela-robot@xiaomi.com>
2021-04-30 03:09:18 +0800

    Snap for 7325308 from 254396820c4363156f7705896b17e9ed2a4fbdce to tm-release
    Change-Id: Ic6836ccae8c468f37b54d515cc07c9417deab48a

--- android_keymaster/android_keymaster.cpp
+++ android_keymaster/android_keymaster.cpp
@@ -82,10 +82,9 @@ constexpr int kP256AffinePointSize = 32;
 
 }  // anonymous namespace
 
-AndroidKeymaster::AndroidKeymaster(KeymasterContext* context, size_t operation_table_size,
-                                   uint32_t message_version)
-    : context_(context), operation_table_(new (std::nothrow) OperationTable(operation_table_size)),
-      message_version_(message_version) {}
+AndroidKeymaster::AndroidKeymaster(KeymasterContext* context, size_t operation_table_size)
+    : context_(context), operation_table_(new (std::nothrow) OperationTable(operation_table_size)) {
+}
 
 AndroidKeymaster::~AndroidKeymaster() {}
 

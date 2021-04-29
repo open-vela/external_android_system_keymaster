6d70e21d74f5589880d6b07ff044661be940ac6f
liujinye <liujinye@xiaomi.com>
2021-04-30 03:09:18 +0800

    Snap for 7325308 from 254396820c4363156f7705896b17e9ed2a4fbdce to tm-release
    Change-Id: Ic6836ccae8c468f37b54d515cc07c9417deab48a

--- include/keymaster/android_keymaster.h
+++ include/keymaster/android_keymaster.h
@@ -46,8 +46,7 @@ class OperationTable;
  */
 class AndroidKeymaster {
   public:
-    AndroidKeymaster(KeymasterContext* context, size_t operation_table_size,
-                     uint32_t message_version = kDefaultMessageVersion);
+    AndroidKeymaster(KeymasterContext* context, size_t operation_table_size);
     virtual ~AndroidKeymaster();
     AndroidKeymaster(AndroidKeymaster&&);
 
@@ -114,7 +113,7 @@ class AndroidKeymaster {
     // If the caller doesn't bother to use GetVersion2 or GetVersion to configure the message
     // version, assume kDefaultVersion, i.e. assume the client and server always support the
     // latest default, which is the latest, except when experimental features are being added.
-    uint32_t message_version_;
+    uint32_t message_version_ = kDefaultMessageVersion;
 };
 
 }  // namespace keymaster

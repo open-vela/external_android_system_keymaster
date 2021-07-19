ff37524f1505dd1888bc3f9c3c2ac5035008a15f
liujinye <liujinye@xiaomi.com>
2021-07-19 18:24:05 +0800

    Don't fail on TAG_ALLOW_WHILE_ON_BODY am: 88baa7828b
    Original change: https://android-review.googlesource.com/c/platform/system/keymaster/+/1764925
    
    Change-Id: Idaf4b1310c1a634dcb75d5f7aa03e5ed623c03c7

--- contexts/pure_soft_remote_provisioning_context.cpp
+++ contexts/pure_soft_remote_provisioning_context.cpp
@@ -17,7 +17,6 @@
 #include <keymaster/contexts/pure_soft_remote_provisioning_context.h>
 
 #include <algorithm>
-#include <android-base/logging.h>
 #include <assert.h>
 
 #include <keymaster/cppcose/cppcose.h>
@@ -29,7 +28,6 @@
 #include <openssl/rand.h>
 
 namespace keymaster {
-namespace {
 
 using cppcose::constructCoseSign1;
 using cppcose::CoseKey;
@@ -40,17 +38,6 @@ using cppcose::VERIFY;
 
 constexpr uint32_t kMacKeyLength = 32;
 
-std::array<uint8_t, 32> GetRandomBytes() {
-    std::array<uint8_t, 32> bytes;
-    // This is used in code paths that cannot fail, so CHECK. If it turns
-    // out that we can actually run out of entropy during thes code paths,
-    // we'll need to refactor the interfaces to allow errors to propagate.
-    CHECK_EQ(RAND_bytes(bytes.data(), bytes.size()), 1) << "Unable to get random bytes";
-    return bytes;
-}
-
-}  // namespace
-
 PureSoftRemoteProvisioningContext::PureSoftRemoteProvisioningContext() {
     std::tie(devicePrivKey_, bcc_) = GenerateBcc(/*testMode=*/false);
 }
@@ -60,7 +47,10 @@ PureSoftRemoteProvisioningContext::~PureSoftRemoteProvisioningContext() {}
 std::vector<uint8_t>
 PureSoftRemoteProvisioningContext::DeriveBytesFromHbk(const std::string& context,
                                                       size_t num_bytes) const {
-    static const std::array<uint8_t, 32> fakeHbk = GetRandomBytes();
+    // bytevec fakeHbk(32, 0);
+    // bytevec result(numBytes);
+    // is it safe to revert the call below back to the call above?
+    std::vector<uint8_t> fakeHbk(32);
     std::vector<uint8_t> result(num_bytes);
 
     // TODO: Figure out if HKDF can fail.  It doesn't seem like it should be able to,
@@ -99,14 +89,14 @@ PureSoftRemoteProvisioningContext::GenerateBcc(bool testMode) const {
     std::vector<uint8_t> privKey(ED25519_PRIVATE_KEY_LEN);
     std::vector<uint8_t> pubKey(ED25519_PUBLIC_KEY_LEN);
 
-    std::array<uint8_t, 32> seed;  // Length is hard-coded in the BoringCrypto API
+    uint8_t seed[32];  // Length is hard-coded in the BoringCrypto API
     if (testMode) {
-        seed = GetRandomBytes();
+        RAND_bytes(seed, sizeof(seed));
     } else {
         auto seed_vector = DeriveBytesFromHbk("Device Key Seed", sizeof(seed));
-        std::copy(seed_vector.begin(), seed_vector.end(), seed.begin());
+        std::copy(seed_vector.begin(), seed_vector.end(), seed);
     }
-    ED25519_keypair_from_seed(pubKey.data(), privKey.data(), seed.data());
+    ED25519_keypair_from_seed(pubKey.data(), privKey.data(), seed);
 
     auto coseKey = cppbor::Map()
                        .add(CoseKey::KEY_TYPE, OCTET_KEY_PAIR)
--- include/keymaster/android_keymaster_messages.h
+++ include/keymaster/android_keymaster_messages.h
@@ -401,7 +401,7 @@ struct GenerateCsrRequest : public KeymasterMessage {
 
     bool test_mode = false;
     size_t num_keys = 0;
-    KeymasterBlob* keys_to_sign_array = nullptr;
+    KeymasterBlob* keys_to_sign_array;
     KeymasterBlob endpoint_enc_cert_chain;
     KeymasterBlob challenge;
 };

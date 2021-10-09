ef306e85aa99d737bd7fe2f1f6850d7c93ee54c2
A. Cody Schuffelen <schuffelen@google.com>
2021-10-08 18:35:39 -0700

    Copy message_version_ in the AndroidKeymaster move constructor
    message_version_ is otherwise unset.
    
    Test: `launch_cvd --noresume` with cuttlefish
    Change-Id: Iddf83c767c230adb090cc4d8dda0901c512d3dc8

--- android_keymaster/android_keymaster.cpp
+++ android_keymaster/android_keymaster.cpp
@@ -138,7 +138,8 @@ AndroidKeymaster::AndroidKeymaster(KeymasterContext* context, size_t operation_t
 AndroidKeymaster::~AndroidKeymaster() {}
 
 AndroidKeymaster::AndroidKeymaster(AndroidKeymaster&& other)
-    : context_(move(other.context_)), operation_table_(move(other.operation_table_)) {}
+    : context_(move(other.context_)), operation_table_(move(other.operation_table_)),
+      message_version_(other.message_version_) {}
 
 void AndroidKeymaster::GetVersion(const GetVersionRequest&, GetVersionResponse* rsp) {
     if (rsp == nullptr) return;

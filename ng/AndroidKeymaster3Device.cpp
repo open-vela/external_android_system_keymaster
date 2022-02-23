810a96d0f710c401eacd23c411f105f52e447ff2
liujinye <liujinye@xiaomi.com>
2022-02-23 18:24:13 +0800

    Provide alternate SE RoT provisioning path. am: 5c4f570ba4
    Original change: https://android-review.googlesource.com/c/platform/system/keymaster/+/1991014
    
    Change-Id: I91ed72b01d87160015d7557dfb08f600170a7761

--- ng/AndroidKeymaster3Device.cpp
+++ ng/AndroidKeymaster3Device.cpp
@@ -21,7 +21,7 @@
 
 #include <android/log.h>
 
-#include "keymaster_tags.h"
+#include "include/authorization_set.h"
 
 #include <keymaster/android_keymaster.h>
 #include <keymaster/android_keymaster_messages.h>

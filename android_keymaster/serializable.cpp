e190d64b99dc4e58d6c95081a2789b6d50940bab
liujinye <liujinye@xiaomi.com>
2022-02-28 15:24:49 +0800

    Clean up some compiler warnings am: ad8d7e39d8 am: 984e211450
    Original change: https://android-review.googlesource.com/c/platform/system/keymaster/+/1984095
    
    Change-Id: I50f0636c163280e3eaacaf9232053635444910ef

--- android_keymaster/serializable.cpp
+++ android_keymaster/serializable.cpp
@@ -22,14 +22,19 @@
 
 namespace keymaster {
 
-bool __buffer_bound_check(const uint8_t* buf, const uint8_t* end, size_t len) {
+namespace {
+
+/* Performs an overflow-checked bounds check */
+bool buffer_bound_check(const uint8_t* buf, const uint8_t* end, size_t len) {
     uintptr_t buf_next;
     bool overflow_occurred = __builtin_add_overflow(__pval(buf), len, &buf_next);
     return (!overflow_occurred) && (buf_next <= __pval(end));
 }
 
+}  // namespace
+
 uint8_t* append_to_buf(uint8_t* buf, const uint8_t* end, const void* data, size_t data_len) {
-    if (__buffer_bound_check(buf, end, data_len)) {
+    if (buffer_bound_check(buf, end, data_len)) {
         memcpy(buf, data, data_len);
         return buf + data_len;
     } else {
@@ -38,7 +43,7 @@ uint8_t* append_to_buf(uint8_t* buf, const uint8_t* end, const void* data, size_
 }
 
 bool copy_from_buf(const uint8_t** buf_ptr, const uint8_t* end, void* dest, size_t size) {
-    if (__buffer_bound_check(*buf_ptr, end, size)) {
+    if (buffer_bound_check(*buf_ptr, end, size)) {
         memcpy(dest, *buf_ptr, size);
         *buf_ptr += size;
         return true;
@@ -56,7 +61,7 @@ bool copy_size_and_data_from_buf(const uint8_t** buf_ptr, const uint8_t* end, si
         return true;
     }
 
-    if (__buffer_bound_check(*buf_ptr, end, *size)) {
+    if (buffer_bound_check(*buf_ptr, end, *size)) {
         dest->reset(new (std::nothrow) uint8_t[*size]);
         if (!dest->get()) {
             return false;

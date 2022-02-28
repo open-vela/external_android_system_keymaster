068914bee7146c8ea4e3ac6ccbebff568ef85b4c
openvela-robot <openvela-robot@xiaomi.com>
2022-02-28 15:02:10 +0800

    Clean up some compiler warnings am: ad8d7e39d8
    Original change: https://android-review.googlesource.com/c/platform/system/keymaster/+/1984095
    
    Change-Id: I558760522e944a0dd9358deb37f223440d13ace8

--- android_keymaster/serializable.cpp
+++ android_keymaster/serializable.cpp
@@ -22,14 +22,19 @@
 
 namespace keymaster {
 
-bool __buffer_bound_check(const uint8_t* buf, const uint8_t* end, size_t len) {
+namespace {
+
+/* Performs an overflow-checked bounds check */
+bool buffer_bound_check(const uint8_t* buf, const uint8_t* end, size_t len) {
     uintptr_t buf_next;
     bool overflow_occurred = __builtin_add_overflow(__pval(buf), len, &buf_next);
     return (!overflow_occurred) && (buf_next <= __pval(end));
 }
 
+}  // namespace
+
 uint8_t* append_to_buf(uint8_t* buf, const uint8_t* end, const void* data, size_t data_len) {
-    if (__buffer_bound_check(buf, end, data_len)) {
+    if (buffer_bound_check(buf, end, data_len)) {
         memcpy(buf, data, data_len);
         return buf + data_len;
     } else {
@@ -38,7 +43,7 @@ uint8_t* append_to_buf(uint8_t* buf, const uint8_t* end, const void* data, size_
 }
 
 bool copy_from_buf(const uint8_t** buf_ptr, const uint8_t* end, void* dest, size_t size) {
-    if (__buffer_bound_check(*buf_ptr, end, size)) {
+    if (buffer_bound_check(*buf_ptr, end, size)) {
         memcpy(dest, *buf_ptr, size);
         *buf_ptr += size;
         return true;
@@ -56,7 +61,7 @@ bool copy_size_and_data_from_buf(const uint8_t** buf_ptr, const uint8_t* end, si
         return true;
     }
 
-    if (__buffer_bound_check(*buf_ptr, end, *size)) {
+    if (buffer_bound_check(*buf_ptr, end, *size)) {
         dest->reset(new (std::nothrow) uint8_t[*size]);
         if (!dest->get()) {
             return false;
@@ -69,6 +74,10 @@ bool copy_size_and_data_from_buf(const uint8_t** buf_ptr, const uint8_t* end, si
 
 bool Buffer::reserve(size_t size) {
     if (available_write() < size) {
+        if (!valid_buffer_state()) {
+            return false;
+        }
+
         size_t new_size = buffer_size_ + size - available_write();
         uint8_t* new_buffer = new (std::nothrow) uint8_t[new_size];
         if (!new_buffer) return false;
@@ -116,6 +125,10 @@ size_t Buffer::available_read() const {
     return write_position_ - read_position_;
 }
 
+bool Buffer::valid_buffer_state() const {
+    return (buffer_size_ >= write_position_) && (write_position_ >= read_position_);
+}
+
 bool Buffer::write(const uint8_t* src, size_t write_length) {
     if (available_write() < write_length) return false;
     memcpy(buffer_.get() + write_position_, src, write_length);
@@ -128,6 +141,21 @@ bool Buffer::read(uint8_t* dest, size_t read_length) {
     memcpy(dest, buffer_.get() + read_position_, read_length);
     read_position_ += read_length;
     return true;
+}
+
+bool Buffer::advance_write(int distance) {
+    if (distance < 0) {
+        return false;
+    }
+
+    const size_t validated_distance = static_cast<size_t>(distance);
+    const size_t new_write_position = write_position_ + validated_distance;
+
+    if (new_write_position <= buffer_size_ && new_write_position >= write_position_) {
+        write_position_ = new_write_position;
+        return true;
+    }
+    return false;
 }
 
 size_t Buffer::SerializedSize() const {

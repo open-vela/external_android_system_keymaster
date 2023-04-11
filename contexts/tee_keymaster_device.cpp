bfafdfb4dd8a5f993571788ea9e7aa9697c06aa4
yangyalei <yangyalei@xiaomi.com>
2023-04-11 17:01:42 +0800

    keymaster: add tee keyamster device impl.
    Tee keymaster devide is a CA that support interface to keystore. We should add a keymaser TA after.
    
    Signed-off-by: yangyalei <yangyalei@xiaomi.com>

---
+++ contexts/tee_keymaster_device.cpp
@@ -0,0 +1,542 @@
+/*
+ * Copyright (C) 2023 Xiaomi Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <common.h>
+#include <keymaster/tee_keymaster_device.h>
+#include <keymaster/contexts/tee_keymaster_ipc.h>
+#include <keymaster/android_keymaster_messages.h>
+
+#define LOG_TAG "TeeKeymasterDevice"
+#include <log/log.h>
+
+#define KEYMASTER_FINSIH_INPUT_LENGHT_MAX 2048
+
+namespace keymaster {
+TeeKeymasterDevice::TeeKeymasterDevice(const hw_module_t* module) {
+    ALOGV("[%s:%d]", __func__, __LINE__);
+    device_ = {};
+
+    device_.common.tag = HARDWARE_DEVICE_TAG;
+    device_.common.version = 1;
+    device_.common.module = const_cast<hw_module_t*>(module);
+    device_.common.close = close_device;
+
+    device_.flags = 0;
+    device_.context = this;
+    device_.configure = configure;
+    device_.add_rng_entropy = add_rng_entropy;
+    device_.generate_key = generate_key;
+    device_.get_key_characteristics = get_key_characteristics;
+    device_.import_key = import_key;
+    device_.export_key = export_key;
+    device_.attest_key = attest_key;
+    device_.upgrade_key = upgrade_key;
+    device_.delete_key = delete_key;
+    device_.delete_all_keys = delete_all_keys;
+    device_.begin = begin;
+    device_.update = update;
+    device_.finish = finish;
+    device_.abort = abort;
+
+    uuid_ = TA_KEYMASTER_UUID;
+    message_version_ = MessageVersion(KmVersion::KEYMASTER_2);
+
+    tee_keymaster_connect(&context_, &session_, &uuid_);
+}
+
+TeeKeymasterDevice::~TeeKeymasterDevice() {
+    ALOGV("[%s:%d]", __func__, __LINE__);
+
+    tee_keymaster_disconnect(&context_, &session_);
+}
+
+namespace {
+
+// Allocates a new buffer with malloc and copies the contents of |buffer| to it.
+// Caller takes ownership of the returned buffer.
+uint8_t* DuplicateBuffer(const uint8_t* buffer, size_t size) {
+    uint8_t* tmp = reinterpret_cast<uint8_t*>(malloc(size));
+    if (tmp) {
+        memcpy(tmp, buffer, size);
+    }
+    return tmp;
+}
+
+template <typename RequestType>
+void AddClientAndAppData(const keymaster_blob_t* client_id,
+                         const keymaster_blob_t* app_data,
+                         RequestType* request) {
+    request->additional_params.Clear();
+    if (client_id && client_id->data_length > 0) {
+        request->additional_params.push_back(TAG_APPLICATION_ID, *client_id);
+    }
+    if (app_data && app_data->data_length > 0) {
+        request->additional_params.push_back(TAG_APPLICATION_DATA, *app_data);
+    }
+}
+
+}  //  unnamed namespace
+
+keymaster_error_t TeeKeymasterDevice::configure(
+    const keymaster_key_param_set_t* params) {
+    ALOGV("[%s:%d]", __func__, __LINE__);
+
+    if (!params) {
+        return KM_ERROR_UNEXPECTED_NULL_POINTER;
+    }
+
+    keymaster_error_t err;
+    AuthorizationSet params_copy(*params);
+    ConfigureRequest request(message_version_);
+    ConfigureResponse response(message_version_);
+
+    if (!params_copy.GetTagValue(TAG_OS_VERSION, &request.os_version) ||
+        !params_copy.GetTagValue(TAG_OS_PATCHLEVEL, &request.os_patchlevel)) {
+        ALOGE("Configuration parameters must contain OS version and patch level");
+        return KM_ERROR_INVALID_ARGUMENT;
+    }
+
+    err = tee_keymaster_send(KM_CONFIGURE, &context_, &session_,
+                             &uuid_, request, &response);
+    if (err != KM_ERROR_OK) {
+        return err;
+    }
+
+    return KM_ERROR_OK;
+}
+
+keymaster_error_t TeeKeymasterDevice::add_rng_entropy(const uint8_t* data,
+                                                      size_t data_length) {
+    return KM_ERROR_UNIMPLEMENTED;
+}
+
+keymaster_error_t TeeKeymasterDevice::generate_key(
+    const keymaster_key_param_set_t* params, keymaster_key_blob_t* key_blob,
+    keymaster_key_characteristics_t* characteristics) {
+    ALOGV("[%s:%d]", __func__, __LINE__);
+
+    if (!params) {
+        return KM_ERROR_UNEXPECTED_NULL_POINTER;
+    }
+    if (!key_blob) {
+        return KM_ERROR_OUTPUT_PARAMETER_NULL;
+    }
+
+    keymaster_error_t err;
+    GenerateKeyRequest request(message_version_);
+    GenerateKeyResponse response(message_version_);
+
+    request.key_description.Reinitialize(*params);
+
+    err = tee_keymaster_send(KM_GENERATE_KEY, &context_, &session_,
+                             &uuid_, request, &response);
+    if (err != KM_ERROR_OK) {
+        return err;
+    }
+
+    key_blob->key_material_size = response.key_blob.key_material_size;
+    key_blob->key_material = DuplicateBuffer(
+        response.key_blob.key_material, response.key_blob.key_material_size);
+    if (!key_blob->key_material) {
+        return KM_ERROR_MEMORY_ALLOCATION_FAILED;
+    }
+
+    if (characteristics) {
+        response.enforced.CopyToParamSet(&characteristics->hw_enforced);
+        response.unenforced.CopyToParamSet(&characteristics->sw_enforced);
+    }
+
+    return KM_ERROR_OK;
+}
+
+keymaster_error_t TeeKeymasterDevice::get_key_characteristics(
+    const keymaster_key_blob_t* key_blob, const keymaster_blob_t* client_id,
+    const keymaster_blob_t* app_data,
+    keymaster_key_characteristics_t* character) {
+    ALOGV("[%s:%d]", __func__, __LINE__);
+
+    if (!key_blob || !key_blob->key_material) {
+        return KM_ERROR_UNEXPECTED_NULL_POINTER;
+    }
+    if (!character) {
+        return KM_ERROR_OUTPUT_PARAMETER_NULL;
+    }
+
+    keymaster_error_t err;
+    GetKeyCharacteristicsRequest request(message_version_);
+    GetKeyCharacteristicsResponse response(message_version_);
+
+    err = tee_keymaster_send(KM_GET_KEY_CHARACTERISTICS, &context_,
+                             &session_, &uuid_, request, &response);
+    if (err != KM_ERROR_OK) {
+        return err;
+    }
+
+    response.enforced.CopyToParamSet(&character->hw_enforced);
+    response.unenforced.CopyToParamSet(&character->sw_enforced);
+
+    return KM_ERROR_OK;
+}
+
+keymaster_error_t TeeKeymasterDevice::import_key(
+    const keymaster_key_param_set_t* params, keymaster_key_format_t key_format,
+    const keymaster_blob_t* key_data, keymaster_key_blob_t* key_blob,
+    keymaster_key_characteristics_t* characteristics) {
+    return KM_ERROR_UNIMPLEMENTED;
+}
+
+keymaster_error_t TeeKeymasterDevice::export_key(
+    keymaster_key_format_t export_format,
+    const keymaster_key_blob_t* key_to_export,
+    const keymaster_blob_t* client_id, const keymaster_blob_t* app_data,
+    keymaster_blob_t* export_data) {
+    return KM_ERROR_UNIMPLEMENTED;
+}
+
+keymaster_error_t TeeKeymasterDevice::attest_key(
+    const keymaster_key_blob_t* key_to_attest,
+    const keymaster_key_param_set_t* attest_params,
+    keymaster_cert_chain_t* cert_chain) {
+    return KM_ERROR_UNIMPLEMENTED;
+}
+
+keymaster_error_t TeeKeymasterDevice::upgrade_key(
+    const keymaster_key_blob_t* key_to_upgrade,
+    const keymaster_key_param_set_t* upgrade_params,
+    keymaster_key_blob_t* upgraded_key) {
+    return KM_ERROR_UNIMPLEMENTED;
+}
+
+keymaster_error_t TeeKeymasterDevice::begin(
+    keymaster_purpose_t purpose, const keymaster_key_blob_t* key,
+    const keymaster_key_param_set_t* in_params,
+    keymaster_key_param_set_t* out_params,
+    keymaster_operation_handle_t* operation_handle) {
+    ALOGV("[%s:%d]", __func__, __LINE__);
+
+    if (!key || !key->key_material) {
+        return KM_ERROR_UNEXPECTED_NULL_POINTER;
+    }
+    if (!operation_handle) {
+        return KM_ERROR_OUTPUT_PARAMETER_NULL;
+    }
+
+    if (out_params) {
+        *out_params = {};
+    }
+
+    keymaster_error_t err;
+    BeginOperationRequest request(message_version_);
+    BeginOperationResponse response(message_version_);
+
+    request.purpose = purpose;
+    request.SetKeyMaterial(*key);
+    request.additional_params.Reinitialize(*in_params);
+
+    err = tee_keymaster_send(KM_BEGIN, &context_, &session_, &uuid_,
+                             request, &response);
+    if (err != KM_ERROR_OK) {
+        return err;
+    }
+
+    if (response.output_params.size() > 0) {
+        if (out_params) {
+            response.output_params.CopyToParamSet(out_params);
+        } else {
+            return KM_ERROR_OUTPUT_PARAMETER_NULL;
+        }
+    }
+    *operation_handle = response.op_handle;
+
+    return KM_ERROR_OK;
+}
+
+keymaster_error_t TeeKeymasterDevice::update(
+    keymaster_operation_handle_t operation_handle,
+    const keymaster_key_param_set_t* in_params, const keymaster_blob_t* input,
+    size_t* input_consumed, keymaster_key_param_set_t* out_params,
+    keymaster_blob_t* output) {
+    ALOGV("[%s:%d]", __func__, __LINE__);
+
+    if (!input) {
+        return KM_ERROR_UNEXPECTED_NULL_POINTER;
+    }
+    if (!input_consumed) {
+        return KM_ERROR_OUTPUT_PARAMETER_NULL;
+    }
+
+    if (out_params) {
+        *out_params = {};
+    }
+    if (output) {
+        *output = {};
+    }
+
+    keymaster_error_t err;
+    UpdateOperationRequest request(message_version_);
+    UpdateOperationResponse response(message_version_);
+
+    request.op_handle = operation_handle;
+    if (in_params) {
+        request.additional_params.Reinitialize(*in_params);
+    }
+    if (input && input->data_length > 0) {
+        size_t max_input_size =
+            KEYMASTER_SEND_BUF_SIZE_MAX - request.SerializedSize();
+        request.input.Reinitialize(
+            input->data, std::min(input->data_length, max_input_size));
+    }
+
+    err = tee_keymaster_send(KM_UPDATE, &context_, &session_, &uuid_,
+                             request, &response);
+    if (err != KM_ERROR_OK) {
+        return err;
+    }
+
+    if (response.output_params.size() > 0) {
+        if (out_params) {
+            response.output_params.CopyToParamSet(out_params);
+        } else {
+            return KM_ERROR_OUTPUT_PARAMETER_NULL;
+        }
+    }
+    *input_consumed = response.input_consumed;
+    if (output) {
+        output->data_length = response.output.available_read();
+        output->data =
+            DuplicateBuffer(response.output.peek_read(), output->data_length);
+        if (!output->data) {
+            return KM_ERROR_MEMORY_ALLOCATION_FAILED;
+        }
+    } else if (response.output.available_read() > 0) {
+        return KM_ERROR_OUTPUT_PARAMETER_NULL;
+    }
+
+    return KM_ERROR_OK;
+}
+
+keymaster_error_t TeeKeymasterDevice::finish(
+    keymaster_operation_handle_t operation_handle,
+    const keymaster_key_param_set_t* in_params, const keymaster_blob_t* input,
+    const keymaster_blob_t* signature, keymaster_key_param_set_t* out_params,
+    keymaster_blob_t* output) {
+    ALOGV("[%s:%d]", __func__, __LINE__);
+
+    if (input && input->data_length > KEYMASTER_FINSIH_INPUT_LENGHT_MAX) {
+        ALOGE("%zu-byte input to finish; only %zu bytes allowed",
+              input->data_length, KEYMASTER_FINSIH_INPUT_LENGHT_MAX);
+        return KM_ERROR_INVALID_INPUT_LENGTH;
+    }
+
+    if (out_params) {
+        *out_params = {};
+    }
+    if (output) {
+        *output = {};
+    }
+
+    keymaster_error_t err;
+    FinishOperationRequest request(message_version_);
+    FinishOperationResponse response(message_version_);
+
+    request.op_handle = operation_handle;
+    if (signature && signature->data && signature->data_length > 0) {
+        request.signature.Reinitialize(signature->data, signature->data_length);
+    }
+    if (input && input->data && input->data_length) {
+        request.input.Reinitialize(input->data, input->data_length);
+    }
+    if (in_params) {
+        request.additional_params.Reinitialize(*in_params);
+    }
+
+    err = tee_keymaster_send(KM_FINISH, &context_, &session_, &uuid_,
+                             request, &response);
+    if (err != KM_ERROR_OK) {
+        return err;
+    }
+
+    if (response.output_params.size() > 0) {
+        if (out_params) {
+            response.output_params.CopyToParamSet(out_params);
+        } else {
+            return KM_ERROR_OUTPUT_PARAMETER_NULL;
+        }
+    }
+    if (output) {
+        output->data_length = response.output.available_read();
+        output->data =
+            DuplicateBuffer(response.output.peek_read(), output->data_length);
+        if (!output->data) {
+            return KM_ERROR_MEMORY_ALLOCATION_FAILED;
+        }
+    } else if (response.output.available_read() > 0) {
+        return KM_ERROR_OUTPUT_PARAMETER_NULL;
+    }
+
+    return KM_ERROR_OK;
+}
+
+keymaster_error_t TeeKeymasterDevice::abort(
+    keymaster_operation_handle_t operation_handle) {
+    return KM_ERROR_UNIMPLEMENTED;
+}
+
+keymaster_error_t TeeKeymasterDevice::delete_key(
+    const keymaster_key_blob_t* key) {
+    ALOGV("[%s:%d]", __func__, __LINE__);
+
+    if (!key || !key->key_material) return KM_ERROR_UNEXPECTED_NULL_POINTER;
+
+    DeleteKeyRequest request(message_version_);
+    DeleteKeyResponse response(message_version_);
+    request.SetKeyMaterial(*key);
+
+    return tee_keymaster_send(KM_DELETE_KEY, &context_, &session_,
+                              &uuid_, request, &response);
+}
+
+keymaster_error_t TeeKeymasterDevice::delete_all_keys() {
+    return KM_ERROR_UNIMPLEMENTED;
+}
+
+static inline TeeKeymasterDevice* convert_device(
+    const keymaster2_device_t* dev) {
+    return reinterpret_cast<TeeKeymasterDevice*>(dev->context);
+}
+
+/* static */
+int TeeKeymasterDevice::close_device(hw_device_t* dev) { return 0; }
+
+/* static */
+keymaster_error_t TeeKeymasterDevice::configure(
+    const keymaster2_device_t* dev, const keymaster_key_param_set_t* params) {
+    return convert_device(dev)->configure(params);
+}
+
+/* static */
+keymaster_error_t TeeKeymasterDevice::add_rng_entropy(
+    const keymaster2_device_t* dev, const uint8_t* data, size_t data_length) {
+    return convert_device(dev)->add_rng_entropy(data, data_length);
+}
+
+/* static */
+keymaster_error_t TeeKeymasterDevice::generate_key(
+    const keymaster2_device_t* dev, const keymaster_key_param_set_t* params,
+    keymaster_key_blob_t* key_blob,
+    keymaster_key_characteristics_t* characteristics) {
+    return convert_device(dev)->generate_key(params, key_blob, characteristics);
+}
+
+/* static */
+keymaster_error_t TeeKeymasterDevice::get_key_characteristics(
+    const keymaster2_device_t* dev, const keymaster_key_blob_t* key_blob,
+    const keymaster_blob_t* client_id, const keymaster_blob_t* app_data,
+    keymaster_key_characteristics_t* characteristics) {
+    return convert_device(dev)->get_key_characteristics(
+        key_blob, client_id, app_data, characteristics);
+}
+
+/* static */
+keymaster_error_t TeeKeymasterDevice::import_key(
+    const keymaster2_device_t* dev, const keymaster_key_param_set_t* params,
+    keymaster_key_format_t key_format, const keymaster_blob_t* key_data,
+    keymaster_key_blob_t* key_blob,
+    keymaster_key_characteristics_t* characteristics) {
+    return convert_device(dev)->import_key(params, key_format, key_data,
+                                           key_blob, characteristics);
+}
+
+/* static */
+keymaster_error_t TeeKeymasterDevice::export_key(
+    const keymaster2_device_t* dev, keymaster_key_format_t export_format,
+    const keymaster_key_blob_t* key_to_export,
+    const keymaster_blob_t* client_id, const keymaster_blob_t* app_data,
+    keymaster_blob_t* export_data) {
+    return convert_device(dev)->export_key(export_format, key_to_export,
+                                           client_id, app_data, export_data);
+}
+
+/* static */
+keymaster_error_t TeeKeymasterDevice::attest_key(
+    const keymaster2_device_t* dev, const keymaster_key_blob_t* key_to_attest,
+    const keymaster_key_param_set_t* attest_params,
+    keymaster_cert_chain_t* cert_chain) {
+    return convert_device(dev)->attest_key(key_to_attest, attest_params,
+                                           cert_chain);
+}
+
+/* static */
+keymaster_error_t TeeKeymasterDevice::upgrade_key(
+    const keymaster2_device_t* dev, const keymaster_key_blob_t* key_to_upgrade,
+    const keymaster_key_param_set_t* upgrade_params,
+    keymaster_key_blob_t* upgraded_key) {
+    return convert_device(dev)->upgrade_key(key_to_upgrade, upgrade_params,
+                                            upgraded_key);
+}
+
+/* static */
+keymaster_error_t TeeKeymasterDevice::begin(
+    const keymaster2_device_t* dev, keymaster_purpose_t purpose,
+    const keymaster_key_blob_t* key, const keymaster_key_param_set_t* in_params,
+    keymaster_key_param_set_t* out_params,
+    keymaster_operation_handle_t* operation_handle) {
+    return convert_device(dev)->begin(purpose, key, in_params, out_params,
+                                      operation_handle);
+}
+
+/* static */
+keymaster_error_t TeeKeymasterDevice::update(
+    const keymaster2_device_t* dev,
+    keymaster_operation_handle_t operation_handle,
+    const keymaster_key_param_set_t* in_params, const keymaster_blob_t* input,
+    size_t* input_consumed, keymaster_key_param_set_t* out_params,
+    keymaster_blob_t* output) {
+    return convert_device(dev)->update(operation_handle, in_params, input,
+                                       input_consumed, out_params, output);
+}
+
+/* static */
+keymaster_error_t TeeKeymasterDevice::finish(
+    const keymaster2_device_t* dev,
+    keymaster_operation_handle_t operation_handle,
+    const keymaster_key_param_set_t* in_params, const keymaster_blob_t* input,
+    const keymaster_blob_t* signature, keymaster_key_param_set_t* out_params,
+    keymaster_blob_t* output) {
+    return convert_device(dev)->finish(operation_handle, in_params, input,
+                                       signature, out_params, output);
+}
+
+/* static */
+keymaster_error_t TeeKeymasterDevice::abort(
+    const keymaster2_device_t* dev,
+    keymaster_operation_handle_t operation_handle) {
+    return convert_device(dev)->abort(operation_handle);
+}
+
+/* static */
+keymaster_error_t TeeKeymasterDevice::delete_key(
+    const keymaster2_device_t* dev, const keymaster_key_blob_t* key) {
+    return convert_device(dev)->delete_key(key);
+}
+
+/* static */
+keymaster_error_t TeeKeymasterDevice::delete_all_keys(
+    const keymaster2_device_t* dev) {
+    return convert_device(dev)->delete_all_keys();
+}
+
+}  // namespace keymaster
---
+++ contexts/tee_keymaster_ipc.cpp
@@ -0,0 +1,177 @@
+/*
+ * Copyright (C) 2023 Xiaomi Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <keymaster/contexts/tee_keymaster_ipc.h>
+#include <keymaster/android_keymaster_messages.h>
+
+#define LOG_TAG "TeeKeymasterIpc"
+#include <log/log.h>
+
+keymaster_error_t tee_keymaster_connect(TEEC_Context* context,
+                                        TEEC_Session* session,
+                                        TEEC_UUID* uuid) {
+    ALOGV("[%s:%d]", __func__, __LINE__);
+
+    uint32_t origin;
+    TEEC_Result res;
+
+    /* Initialize a context connecting us to the TEE */
+    res = TEEC_InitializeContext(NULL, context);
+    if (res != TEEC_SUCCESS) {
+        ALOGE("TEEC_InitializeContext failed with code 0x%08x\n", res);
+        return translate_error(res);
+    }
+
+    /* Open session with the TA */
+    res = TEEC_OpenSession(context, session, uuid, TEEC_LOGIN_PUBLIC, NULL,
+                           NULL, &origin);
+    if (res != TEEC_SUCCESS) {
+        ALOGE("TEEC_Opensession failed with code 0x%08x origin 0x%08x\n", res,
+              origin);
+        return translate_error(res);
+    }
+
+    return KM_ERROR_OK;
+}
+
+void tee_keymaster_disconnect(TEEC_Context* context, TEEC_Session* session) {
+    ALOGV("[%s:%d]", __func__, __LINE__);
+
+    TEEC_CloseSession(session);
+    TEEC_FinalizeContext(context);
+}
+
+keymaster_error_t translate_error(TEEC_Result err) {
+    switch (err) {
+        case TEEC_SUCCESS:
+            return KM_ERROR_OK;
+        case TEEC_ERROR_ACCESS_DENIED:
+        case TEEC_ERROR_ACCESS_CONFLICT:
+        case TEEC_ERROR_SECURITY:
+            return KM_ERROR_SECURE_HW_ACCESS_DENIED;
+
+        case TEEC_ERROR_CANCEL:
+        case TEEC_ERROR_EXTERNAL_CANCEL:
+            return KM_ERROR_OPERATION_CANCELLED;
+
+        case TEEC_ERROR_NOT_IMPLEMENTED:
+            return KM_ERROR_UNIMPLEMENTED;
+
+        case TEEC_ERROR_OUT_OF_MEMORY:
+            return KM_ERROR_MEMORY_ALLOCATION_FAILED;
+
+        case TEEC_ERROR_BUSY:
+            return KM_ERROR_SECURE_HW_BUSY;
+
+        case TEEC_ERROR_COMMUNICATION:
+            return KM_ERROR_SECURE_HW_COMMUNICATION_FAILED;
+
+        case TEEC_ERROR_SHORT_BUFFER:
+            return KM_ERROR_INVALID_INPUT_LENGTH;
+
+        default:
+            return KM_ERROR_UNKNOWN_ERROR;
+    }
+}
+
+keymaster_error_t tee_keymaster_send(keystore_command command,
+                                     TEEC_Context* context,
+                                     TEEC_Session* session, TEEC_UUID* uuid,
+                                     const keymaster::Serializable& request,
+                                     keymaster::KeymasterResponse* response) {
+    ALOGV("[%s:%d][%d]", __func__, __LINE__, command);
+
+    uint32_t req_size = request.SerializedSize();
+    if (req_size > KEYMASTER_SEND_BUF_SIZE_MAX) {
+        ALOGE("Request too big: %u Max size: %u", req_size, KEYMASTER_SEND_BUF_SIZE_MAX);
+        return KM_ERROR_INVALID_INPUT_LENGTH;
+    }
+
+    TEEC_Result res;
+    TEEC_Operation op;
+    uint32_t origin;
+    uint8_t* buffer;
+    TEEC_SharedMemory req_shm;
+    TEEC_SharedMemory rsp_shm;
+    keymaster_error_t keymaster_res = KM_ERROR_OK;
+
+    // construct req_shm
+    req_shm.size = request.SerializedSize();
+    req_shm.flags = TEEC_MEM_INPUT;
+
+    res = TEEC_AllocateSharedMemory(context, &req_shm);
+    if (res != TEEC_SUCCESS) {
+        ALOGE("[%d]TEEC_AllocateSharedMemory failed with code 0x%08x\n",
+              command, res);
+        goto exit;
+    }
+    memset(req_shm.buffer, 0, req_shm.size);
+    request.Serialize((uint8_t*)req_shm.buffer,
+                      (uint8_t*)req_shm.buffer + req_shm.size);
+
+    // construct rsp_shm
+    rsp_shm.size = KEYMASTER_RECE_BUF_SIZE_MAX;
+    rsp_shm.flags = TEEC_MEM_OUTPUT;
+    res = TEEC_AllocateSharedMemory(context, &rsp_shm);
+    if (res != TEEC_SUCCESS) {
+        ALOGE("[%d]TEEC_AllocateSharedMemory failed with code 0x%08x\n",
+              command, res);
+        goto exit_free_req_mem;
+    }
+    memset(rsp_shm.buffer, 0, rsp_shm.size);
+
+    // construct op
+    memset(&op, 0, sizeof(op));
+    op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_WHOLE, TEEC_MEMREF_WHOLE,
+                                     TEEC_NONE, TEEC_NONE);
+    op.params[0].memref.parent = &req_shm;
+    op.params[1].memref.parent = &rsp_shm;
+
+    // run command
+    res = TEEC_InvokeCommand(session, command, &op, &origin);
+    if (res != TEEC_SUCCESS) {
+        ALOGE(
+            "[%d]TEEC_InvokeCommand failed with code 0x%08x origin 0x%08x, "
+            "reconnect automatic.\n",
+            command, res, origin);
+        tee_keymaster_disconnect(context, session);
+        tee_keymaster_connect(context, session, uuid);
+        goto exit_free_mem;
+    }
+
+    // deconstruct response
+    buffer = (uint8_t*)rsp_shm.buffer;
+    if (!response->Deserialize((const uint8_t**)&buffer,
+                               buffer + rsp_shm.size)) {
+        ALOGE("[%d]Error deserializing response of size %d\n", command,
+              (int)rsp_shm.size);
+        keymaster_res = KM_ERROR_UNKNOWN_ERROR;
+    } else if (response->error != KM_ERROR_OK) {
+        ALOGE("[%d]Response of size %d contained error code %d\n", command,
+              (int)rsp_shm.size, (int)response->error);
+        keymaster_res = response->error;
+    }
+
+exit_free_mem:
+    TEEC_ReleaseSharedMemory(&rsp_shm);
+exit_free_req_mem:
+    TEEC_ReleaseSharedMemory(&req_shm);
+exit:
+    if (keymaster_res != KM_ERROR_OK) {
+        return keymaster_res;
+    }
+    return translate_error(res);
+}

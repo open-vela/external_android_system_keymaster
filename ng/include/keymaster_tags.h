a5b85d4b7b14b7f7a7f4d64eb25e75f37813bb8e
guoshichao <guoshichao@xiaomi.com>
2023-04-18 15:20:15 +0800

    keymaster: exclude the IHwKeymasterDevice.h file from keymaster_tag
    the IHwKeymasterDevice.h needs the hwbinder/IBinder.h, we do not
    integrate the hwbinder, so need to exclude this IHwKeymasterDevice.h
    file.
    
    Signed-off-by: guoshichao <guoshichao@xiaomi.com>

--- ng/include/keymaster_tags.h
+++ ng/include/keymaster_tags.h
@@ -58,7 +58,6 @@
  * value1.isOk() yields false, but value2.isOk() yields true, thus value2.value() is save to access.
  */
 
-#include <android/hardware/keymaster/3.0/IHwKeymasterDevice.h>
 #include <hardware/hw_auth_token.h>
 #include <type_traits>
 
